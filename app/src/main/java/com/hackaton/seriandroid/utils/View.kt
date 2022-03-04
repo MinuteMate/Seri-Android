3673e118c3a39abf3a7d8f18d45f055cef990bda
임세현 <sh00710000@gmail.com>
2022-03-05 00:24:48 +0900

    📑 :: utils

---
+++ app/src/main/java/com/hackaton/seriandroid/utils/View.kt
@@ -0,0 +1,37 @@
+package com.semicolon.walkhub.util
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun View.isVisible() = this.visibility == View.VISIBLE
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.GONE
+}
+
+fun ImageView.loadFromUrl(url: String) =
+    Glide.with(this.context.applicationContext)
+        .load(url)
+        .into(this)
+
+fun EditText.onTextChanged(block: (s: CharSequence?, start: Int, before: Int, count: Int) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            block(s, start, before, count)
+        }
+    })
+}

c0d29e10b71f470360bc92048bba5dab4b11fb6b
ckrudals <2004ckck@naver.com>
2022-03-05 14:32:25 +0900

    ⚡️ :: AuthLogin 생성

--- app/src/main/java/com/hackaton/seriandroid/ui/auth/login/AuthLoginFragment.kt
+++ app/src/main/java/com/hackaton/seriandroid/ui/auth/login/AuthLoginFragment.kt
@@ -5,6 +5,9 @@ import com.hackaton.seriandroid.R
 import com.hackaton.seriandroid.databinding.FragmentAuthLoginBinding
 import com.hackaton.seriandroid.ui.base.BaseBottomSheetDialogFragment
 import com.hackaton.seriandroid.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 
 class AuthLoginFragment : BaseBottomSheetDialogFragment<FragmentAuthLoginBinding>(R.layout.fragment_auth_login){
     override fun initView() {

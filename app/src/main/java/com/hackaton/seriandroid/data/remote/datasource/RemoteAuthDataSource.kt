767e962e1afafa469541b6e1c0915764941aefc4
임세현 <sh00710000@gmail.com>
2022-03-05 13:44:46 +0900

    ⚡️ :: remote auth datasource

--- app/src/main/java/com/hackaton/seriandroid/data/remote/datasource/RemoteAuthDataSource.kt
+++ app/src/main/java/com/hackaton/seriandroid/data/remote/datasource/RemoteAuthDataSource.kt
@@ -1,4 +1,28 @@
 package com.hackaton.seriandroid.data.remote.datasource
 
+import com.hackaton.seriandroid.data.remote.dto.request.*
+import com.hackaton.seriandroid.data.remote.dto.response.*
+import com.hackaton.seriandroid.data.remote.network.ResultWrapper
+
 interface RemoteAuthDataSource {
+
+    suspend fun signIn(signInRequest: SignInRequest): ResultWrapper<SignInResponse?>
+
+    suspend fun reissueToken(refreshToken: String): ResultWrapper<ReissueTokenResponse?>
+
+    suspend fun signUp(signUpRequest: SignUpRequest): ResultWrapper<SignUpResponse?>
+
+    suspend fun sendEmail(email: String): ResultWrapper<Unit?>
+
+    suspend fun verifyEmail(number: String): ResultWrapper<FetchVerifyCodeResponse?>
+
+    suspend fun checkSignInEmailPolicy(email: String): ResultWrapper<Unit?>
+
+    suspend fun checkSignInPasswordPolicy(password: String): ResultWrapper<Unit?>
+
+    suspend fun checkSignUpEmailPolicy(email: String): ResultWrapper<Unit?>
+
+    suspend fun checkSignUpNicknamePolicy(nickname: String): ResultWrapper<Unit?>
+
+    suspend fun checkSignUpPasswordPolicy(password: String): ResultWrapper<Unit?>
 }
